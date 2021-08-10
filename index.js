0da8a58032fb17be14d3a5263f1fe3bf461189ec
William Mosto <will.mosto@gmail.com>
2021-08-10 19:36:37 -0400

    commit tree d1442aa23dcf173097f183c3aad231eaba4a9272

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 
-
-function writeCards(name,event) {
-  const thankYouCards = [];
-    for (let x = 0; x < name.length; x++) {
-        thankYouCards.push(`Thank you, ${name[x]}, for the wonderful ${event} gift!`);
-        //debugger;
-    }
-    return thankYouCards;
+const writeCards = (name,event) => {
+  let newArray = []
+  for(let e of name) {
+     newArray.push(`Thank you, ${e}, for the wonderful ${event} gift!`)  
+  }
+  return newArray
 }
 
+const countDown = num => {
+  for(let i = num; i>=0; i--) {
+    console.log(i);
+  }
+}
+countDown(10);
+
+////do it again /////
+// function writeCards(name,event) {
+//   const thankYouCards = [];
+//     for (let x = 0; x < name.length; x++) {
+//         thankYouCards.push(`Thank you, ${name[x]}, for the wonderful ${event} gift!`);
+//         //debugger;
+//     }
+//     return thankYouCards;
+// }
 
 
 
 
-function countDown(i) {
-  while (i >= 0) {
-    console.log(i--);
-  } 
-} 
+
+// function countDown(i) {
+//   while (i >= 0) {
+//     console.log(i--);
+//   } 
+// } 
 
 
 
